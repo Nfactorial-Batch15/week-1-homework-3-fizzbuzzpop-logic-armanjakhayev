a186c941ba1055507bb8c9dc58f931c2ca6df11f
armanjakhayev <67386397+armanjakhayev@users.noreply.github.com>
2021-12-05 01:29:38 -0800

    Initial Commit

--- HW3/main.swift
+++ HW3/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Input your number")
+let number = Int(readLine()!)
 
+var result = " "
+
+if number! % 105 == 0 {
+    result = "FizzBuzzPop"
+    print(result)
+} else if number! % 35 == 0 {
+    result = "BuzzPop"
+    print(result)
+}else if number! % 21 == 0 {
+    result = "FizzPop"
+    print(result)
+}else if number! % 15 == 0 {
+    result = "FizzBuzz"
+    print(result)
+} else if number! % 7 == 0 {
+    result = "Pop"
+    print(result)
+}else if number! % 5 == 0 {
+    result = "Buzz"
+    print(result)
+}else if number! % 3 == 0 {
+    result = "Fizz"
+    print(result)
+}else {
+    print("the number is not divisible by 3, 5 or 7")
+}
